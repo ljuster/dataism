13f1bf8e102e8c57cc57fb7ebd935ddcd120c4d3
leora <leora@orlinresearch.com>
2016-11-11 15:32:46 -0800

    navbar

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+MainController < ApplicationController
+  
+
+  # GET /main
+  # GET /purchases.json
+  def index
+
+  end
+
+  # GET /purchases/1
+  # GET /purchases/1.json
+  def show
+
+  end
+
+
