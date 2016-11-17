7734cf813357f7fe8158646914128e11b7afd6ec
leora <leora@orlinresearch.com>
2016-11-17 10:19:49 -0800

    fix palindrome case of uppercase

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -1,7 +1,7 @@
 class Algorithm < ActiveRecord::Base
 
   def isPalindrome()
-    input = self[:input]
+    input = self[:input].downcase
     startTime = Time.now
     for i in 0..input.length/2
       if input[i]!=input[input.length-1-i]
