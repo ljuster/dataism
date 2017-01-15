c5d92cd0dd1f5885959b79b61d2fc32c4236d454
Leora Juster <leorajuster@gmail.com>
2017-01-15 12:21:00 -0800

    rServe

--- init.R
+++ init.R
@@ -12,3 +12,11 @@ install_if_missing = function(p) {
 }
 
 invisible(sapply(my_packages, install_if_missing))
+
+require('Rserve')
+
+# get the port allowed
+port <- Sys.getenv('PORT')
+
+# run Rserve in process
+run.Rserve(debug = TRUE, port, args = NULL, config.file = "./rserve.conf")
