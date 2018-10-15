dee165fbe90adbc05dc4842730530fffbf14438a
Leora Juster <leora@resortpass.com>
2018-10-14 20:06:28 -0700

    rm reason

--- client/app/bundles/HelloWorld/hello-world-bundle.js
+++ client/app/bundles/HelloWorld/hello-world-bundle.js
@@ -2,7 +2,6 @@ import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from './HelloWorldJsx/components/HelloWorld';
 import HelloTypescript from './HelloTypescript/components/HelloTypescript';
-import './HelloReason/components/HelloReson.bs';
 
 
 // This is how react_on_rails can see the HelloWorld in the browser.
